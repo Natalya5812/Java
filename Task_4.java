f09c8e054c087849832ffcd825835f87ec74eaad
Natalya5812 <93934726+Natalya5812@users.noreply.github.com>
2022-06-27 21:06:23 +0400

    Update Task_4.java

--- Task_4.java
+++ Task_4.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Написать программу, показывающую последовательность действий для игры “Ханойская башня”
 
 public class Task_4 {
@@ -25,31 +24,4 @@ public class Task_4 {
    }
 }    
 }
-=======
-// Написать программу, показывающую последовательность действий для игры “Ханойская башня”
 
-public class Task_4 {
-    public static void main(String[] args) {
-        int n = 8;
-        String pos1 = "Стержень 1";
-        String pos2 = "Стержень 2";
-        String pos3 = "Стержень 3";
-
-        hanoy(n, pos1, pos2, pos3);
-    }
-        
-    public static void move (String pos1, String pos2) {
-            System.out.print (pos1 + " => " + pos2 + "\n"); 
-}
-
-    public static void hanoy(int n,String pos1,String pos2,String pos3) {
-   if(n == 1) {
-       move(pos1,pos3);  
-   } else {
-       hanoy(n-1,pos1,pos3,pos2);
-       move(pos1,pos3);
-       hanoy(n-1,pos2,pos1,pos3);
-   }
-}    
-}
->>>>>>> c961b2d769edb6529b239fe6f8ddc06b45758ecf
